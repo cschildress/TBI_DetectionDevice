102713d166045fda88418b4f0f1afa9fcabc39c1
cschildress <calchildress23@gmail.com>
2020-03-01 16:49:05 -0500

    working stroop

--- StroopPictures.py
+++ StroopPictures.py
@@ -18,12 +18,14 @@ class Ui_StroopPictures(object):
     quad2 = [None] * 10
     quad3 = [None] * 10
     quad4 = [None] * 10
+    targetColor = ["#dd1fe7", "#6d25d9", "#da2026", "#6d25d9", "#000000", "#d87c48", "#ffcf49", "#1827b7", "#56bc5f"]
+    targetName = ["BLUE", "PURPLE", "GREEN", "PURPLE", "YELLOW", "ORANGE", "YELLOW", "RED", "GREEN"]
 
-    for x in range(1,11):
-        quad1[x - 1] = "/Users/CABOOMDUDE23/Downloads/PythonStuff/T" + str(x) + "S1" + ".jpg"
-        quad2[x - 1] = "/Users/CABOOMDUDE23/Downloads/PythonStuff//T" + str(x) + "S2" + ".jpg"
-        quad3[x - 1] = "/Users/CABOOMDUDE23/Downloads/PythonStuff//T" + str(x) + "S3" + ".jpg"
-        quad4[x - 1] = "/Users/CABOOMDUDE23/Downloads/PythonStuff//T" + str(x) + "S4" + ".jpg"
+    for x in range(1, 11):
+        quad1[x - 1] = "/home/pi/StroopPictures/T" + str(x) + "S1" + ".jpg"
+        quad2[x - 1] = "/home/pi/StroopPictures/T" + str(x) + "S2" + ".jpg"
+        quad3[x - 1] = "/home/pi/StroopPictures/T" + str(x) + "S3" + ".jpg"
+        quad4[x - 1] = "/home/pi/StroopPictures/T" + str(x) + "S4" + ".jpg"
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -31,37 +33,48 @@ class Ui_StroopPictures(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(160, 70, 451, 341))
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(190, 70, 401, 341))
         self.gridLayoutWidget.setObjectName("gridLayoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
-        self.image1 = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.image1.setText("")
-        self.image1.setPixmap(QtGui.QPixmap(self.quad1[0]))
-        self.image1.setScaledContents(True)
-        self.image1.setObjectName("image1")
-        self.gridLayout.addWidget(self.image1, 0, 0, 1, 1)
-        self.image2 = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.image2.setText("")
-        self.image2.setPixmap(QtGui.QPixmap(self.quad2[0]))
-        self.image2.setScaledContents(True)
-        self.image2.setObjectName("image2")
-        self.gridLayout.addWidget(self.image2, 1, 0, 1, 1)
-        self.label_4 = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.label_4.setText("")
-        self.label_4.setPixmap(QtGui.QPixmap(self.quad3[0]))
-        self.label_4.setScaledContents(True)
-        self.label_4.setObjectName("label_4")
-        self.gridLayout.addWidget(self.label_4, 1, 1, 1, 1)
-        self.image3 = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.image3.setText("")
-        self.image3.setPixmap(QtGui.QPixmap(self.quad4[0]))
-        self.image3.setScaledContents(True)
-        self.image3.setObjectName("image3")
-        self.gridLayout.addWidget(self.image3, 0, 1, 1, 1)
+        self.gridLayout_2 = QtWidgets.QGridLayout()
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout_2.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout_2.addWidget(self.pushButton_2, 1, 0, 1, 1)
+        self.pushButton_3 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
+        self.pushButton_3.setSizePolicy(sizePolicy)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout_2.addWidget(self.pushButton_3, 0, 1, 1, 1)
+        self.pushButton_4 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
+        self.pushButton_4.setSizePolicy(sizePolicy)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.gridLayout_2.addWidget(self.pushButton_4, 1, 1, 1, 1)
+        self.gridLayout.addLayout(self.gridLayout_2, 0, 0, 1, 1)
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(290, 0, 201, 51))
+        self.textBrowser.setGeometry(QtCore.QRect(310, 0, 171, 51))
         self.textBrowser.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.textBrowser.setObjectName("textBrowser")
         MainWindow.setCentralWidget(self.centralwidget)
@@ -76,58 +89,63 @@ class Ui_StroopPictures(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.clickable(self.image1).connect(self.buildStroop)
-        self.clickable(self.image2).connect(self.buildStroop)
-        self.clickable(self.image3).connect(self.buildStroop)
-        self.clickable(self.image4).connect(self.buildStroop)
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:26pt; font-weight:600; color:#4eab5a;\">ORANGE</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", ""))
+        self.pushButton_2.setText(_translate("MainWindow", ""))
+        self.pushButton_3.setText(_translate("MainWindow", ""))
+        self.pushButton_4.setText(_translate("MainWindow", ""))
+        self.textBrowser.setHtml(_translate("MainWindow",
+                                            "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                            "p, li { white-space: pre-wrap; }\n"
+                                            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                            "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:26pt; font-weight:600; color:#4eab5a;\">ORANGE</span></p></body></html>"))
+        self.textBrowser.setAutoFillBackground(True)
 
-    def clickable(widget):
+        self.pushButton.setIcon(QtGui.QIcon(self.quad1[0]))
+        self.pushButton.setIconSize(QtCore.QSize(300, 180))
+        self.pushButton.clicked.connect(self.buildStroop)
+        self.pushButton.setFlat(True)
 
-        class Filter(QObject):
+        self.pushButton_2.setIcon(QtGui.QIcon(self.quad2[0]))
+        self.pushButton_2.setIconSize(QtCore.QSize(300, 180))
+        self.pushButton_2.clicked.connect(self.buildStroop)
+        self.pushButton_2.setFlat(True)
 
-            clicked = pyqtSignal()
-            def eventFilter(self, obj, event):
-                if obj == widget:
-                    if event.type() == QEvent.MouseButtonRelease:
-                        if obj.rect().contains(event.pos()):
-                            self.clicked.emit()
-                            return True
-                return False
+        self.pushButton_3.setIcon(QtGui.QIcon(self.quad3[0]))
+        self.pushButton_3.setIconSize(QtCore.QSize(300, 180))
+        self.pushButton_3.clicked.connect(self.buildStroop)
+        self.pushButton_3.setFlat(True)
 
-        filter = Filter(widget)
-        widget.installEventFilter
-        return filter.clicked
-    #
-    # class Window(QWidget):
-    #     print("test")
-    #     def __init__(self, parent=None):
-    #         QWidget.__init__(self, parent)
-    #
-    #
-
-
+        self.pushButton_4.setIcon(QtGui.QIcon(self.quad4[0]))
+        self.pushButton_4.setIconSize(QtCore.QSize(300, 180))
+        self.pushButton_4.clicked.connect(self.buildStroop)
+        self.pushButton_4.setFlat(True)
 
     def buildStroop(self):
-        print("test ")
         self.count += 1
-        self.image1.setPixmap(QtGui.QPixmap(self.quad1[self.count]))
-        self.image2.setPixmap(QtGui.QPixmap(self.quad2[self.count]))
-        self.image3.setPixmap(QtGui.QPixmap(self.quad3[self.count]))
-        self.image4.setPixmap(QtGui.QPixmap(self.quad4[self.count]))
-
+        self.pushButton.setIcon(QtGui.QIcon(self.quad1[self.count]))
+        self.pushButton.setIconSize(QtCore.QSize(326, 196))
+        self.pushButton_2.setIcon(QtGui.QIcon(self.quad2[self.count]))
+        self.pushButton_2.setIconSize(QtCore.QSize(326, 196))
+        self.pushButton_3.setIcon(QtGui.QIcon(self.quad3[self.count]))
+        self.pushButton_3.setIconSize(QtCore.QSize(326, 196))
+        self.pushButton_4.setIcon(QtGui.QIcon(self.quad4[self.count]))
+        self.pushButton_4.setIconSize(QtCore.QSize(326, 196))
+        _translate = QtCore.QCoreApplication.translate
+        self.textBrowser.setHtml(_translate("MainWindow",
+                                            "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                            "p, li { white-space: pre-wrap; }\n"
+                                            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                            "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:26pt; font-weight:600; color: " + self.targetColor[self.count - 1] + "\">" + self.targetName[self.count - 1] + "</span></p></body></html>"))
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_StroopPictures()
